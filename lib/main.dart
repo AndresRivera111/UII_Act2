a873bb0e7aae1f755231b0715c609c0e0639bfac
ANDRES_MIGUEL_RIVERA_FRANCISCO <a.22308051281295@cbtis128.edu.mx>
2025-03-20 18:56:51 +0000

    Act 12

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,59 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'ANDRES RIVERA | 1295',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.orange,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                letterSpacing: 1,
+              ),
+            ),
+          ),
+        ),
+        body: Center(
+          child: Card(
+            elevation: 5.0,
+            margin: EdgeInsets.all(16.0),
+            color: Color(0xfffedc76),
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment
+                    .start, // Alinea el contenido a la izquierda
+                children: <Widget>[
+                  Text(
+                    'Información de la Marca',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff011e36),
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Text('Nombre: Juguetes Creativos S.A.',
+                      style: TextStyle(fontSize: 16.0)),
+                  Text('Edad: 25 años', style: TextStyle(fontSize: 16.0)),
+                  Text('Dirección: Calle Juguete 123, Ciudad Diversión',
+                      style: TextStyle(fontSize: 16.0)),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
